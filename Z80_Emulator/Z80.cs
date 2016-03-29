82891a15c9a6027a8ca5960719a3fae6ba08a24d
Hani Mounla <hani-mounla@live.com>
2016-03-29 21:54:45 +0200

    handle the bit b,r method and regioned it :) ... must region all the shit

--- Z80_Emulator/Z80.cs
+++ Z80_Emulator/Z80.cs
@@ -1094,133 +1094,133 @@ namespace Z80_Emulator
                     break;
                 case 0x3F: SRL(A, 1);
                     break;
-                case 0x40:
+                case 0x40: BIT0(B);
                     break;
-                case 0x41:
+                case 0x41: BIT0(C);
                     break;
-                case 0x42:
+                case 0x42: BIT0(D);
                     break;
-                case 0x43:
+                case 0x43: BIT0(E);
                     break;
-                case 0x44:
+                case 0x44: BIT0(H);
                     break;
-                case 0x45:
+                case 0x45: BIT0(L);
                     break;
-                case 0x46:
+                case 0x46: BIT0(Memory[HL]);
                     break;
-                case 0x47:
+                case 0x47: BIT0(A);
                     break;
-                case 0x48:
+                case 0x48: BIT1(B);
                     break;
-                case 0x49:
+                case 0x49: BIT1(C);
                     break;
-                case 0x4A:
+                case 0x4A: BIT1(D);
                     break;
-                case 0x4B:
+                case 0x4B: BIT1(E);
                     break;
-                case 0x4C:
+                case 0x4C: BIT1(H);
                     break;
-                case 0x4D:
+                case 0x4D: BIT1(L);
                     break;
-                case 0x4E:
+                case 0x4E: BIT1(Memory[HL]);
                     break;
-                case 0x4F:
+                case 0x4F: BIT1(A);
                     break;
-                case 0x50:
+                case 0x50: BIT2(B);
                     break;
-                case 0x51:
+                case 0x51: BIT2(C);
                     break;
-                case 0x52:
+                case 0x52: BIT2(D);
                     break;
-                case 0x53:
+                case 0x53: BIT2(E);
                     break;
-                case 0x54:
+                case 0x54: BIT2(H);
                     break;
-                case 0x55:
+                case 0x55: BIT2(L);
                     break;
-                case 0x56:
+                case 0x56: BIT2(Memory[HL]);
                     break;
-                case 0x57:
+                case 0x57: BIT2(A);
                     break;
-                case 0x58:
+                case 0x58: BIT3(B);
                     break;
-                case 0x59:
+                case 0x59: BIT3(C);
                     break;
-                case 0x5A:
+                case 0x5A: BIT3(D);
                     break;
-                case 0x5B:
+                case 0x5B: BIT3(E);
                     break;
-                case 0x5C:
+                case 0x5C: BIT3(H);
                     break;
-                case 0x5D:
+                case 0x5D: BIT3(L);
                     break;
-                case 0x5E:
+                case 0x5E: BIT3(Memory[HL]);
                     break;
-                case 0x5F:
+                case 0x5F: BIT3(A);
                     break;
-                case 0x60:
+                case 0x60: BIT4(B);
                     break;
-                case 0x61:
+                case 0x61: BIT4(C);
                     break;
-                case 0x62:
+                case 0x62: BIT4(D);
                     break;
-                case 0x63:
+                case 0x63: BIT4(E);
                     break;
-                case 0x64:
+                case 0x64: BIT4(H);
                     break;
-                case 0x65:
+                case 0x65: BIT4(L);
                     break;
-                case 0x66:
+                case 0x66: BIT4(Memory[HL]);
                     break;
-                case 0x67:
+                case 0x67: BIT4(A);
                     break;
-                case 0x68:
+                case 0x68: BIT5(B);
                     break;
-                case 0x69:
+                case 0x69: BIT5(C);
                     break;
-                case 0x6A:
+                case 0x6A: BIT5(D);
                     break;
-                case 0x6B:
+                case 0x6B: BIT5(E);
                     break;
-                case 0x6C:
+                case 0x6C: BIT5(H);
                     break;
-                case 0x6D:
+                case 0x6D: BIT5(L);
                     break;
-                case 0x6E:
+                case 0x6E: BIT5(Memory[HL]);
                     break;
-                case 0x6F:
+                case 0x6F: BIT5(A);
                     break;
-                case 0x70:
+                case 0x70: BIT6(B);
                     break;
-                case 0x71:
+                case 0x71: BIT6(C);
                     break;
-                case 0x72:
+                case 0x72: BIT6(D);
                     break;
-                case 0x73:
+                case 0x73: BIT6(E);
                     break;
-                case 0x74:
+                case 0x74: BIT6(H);
                     break;
-                case 0x75:
+                case 0x75: BIT6(L);
                     break;
-                case 0x76:
+                case 0x76: BIT6(Memory[HL]);
                     break;
-                case 0x77:
+                case 0x77: BIT6(A);
                     break;
-                case 0x78:
+                case 0x78: BIT7(B);
                     break;
-                case 0x79:
+                case 0x79: BIT7(C);
                     break;
-                case 0x7A:
+                case 0x7A: BIT7(D);
                     break;
-                case 0x7B:
+                case 0x7B: BIT7(E);
                     break;
-                case 0x7C:
+                case 0x7C: BIT7(H);
                     break;
-                case 0x7D:
+                case 0x7D: BIT7(L);
                     break;
-                case 0x7E:
+                case 0x7E: BIT7(Memory[HL]);
                     break;
-                case 0x7F:
+                case 0x7F: BIT7(A);
                     break;
                 case 0x80:
                     break;
@@ -1481,6 +1481,47 @@ namespace Z80_Emulator
             }
             
         }
+        #region bit b,r  // will do logical OR with selected bit , if 0 then keep the Z flag on 1
+        private void BIT7(byte val)
+        {
+            SetFlag(Flags.Z, (val |= 0x80) == 0); // 1000 0000 MSB 
+        }
+
+        private void BIT6(byte val)
+        {
+            SetFlag(Flags.Z, (val |= 0x40) == 0); // 0100 0000
+        }
+
+        private void BIT5(byte val)
+        {
+            SetFlag(Flags.Z, (val |= 0x20) == 0); // 0010 0000
+        }
+
+        private void BIT4(byte val)
+        {
+            SetFlag(Flags.Z, (val |= 0x10) == 0); // 0001 0000
+        }
+
+        private void BIT3(byte val)
+        {
+            SetFlag(Flags.Z, (val |= 0x08) == 0); // 0000 1000
+        }
+
+        private void BIT2(byte val)
+        {
+            SetFlag(Flags.Z, (val |= 0x04) == 0); // 0000 0100
+        }
+
+        private void BIT1(byte val)
+        {
+            SetFlag(Flags.Z, (val |= 0x02) == 0); // 0000 0010
+        }
+
+        private void BIT0(byte val)
+        {
+            SetFlag(Flags.Z, (val |= 0x01) == 0); // LSB 0000 0001
+        }
+        #endregion
 
         private void Handle_ED()
         {
